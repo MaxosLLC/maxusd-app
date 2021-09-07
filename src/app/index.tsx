47c443e501caab0d020d613e2433c3c985efc302
boltsr <skrullweb.dev@gmail.com>
2021-09-06 21:57:20 -0400

    initialize Dashboard page

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Dashboard}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/app/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Header } from '../../components/Header';
+export function Dashboard() {
+    return(
+        <>
+            <Header/>
+            <div>asdfade</div>
+        </>
+    );
+}
